989d8b28ad973df873cb210f5bbdfb4e60c01bd2
ChrisNewmanGDL <83370908+ChrisNewmanGDL@users.noreply.github.com>
2021-08-30 17:56:32 +0000

    Fixed attack damage error so output matches, fixed formatting

--- DefensiveItem.cpp
+++ DefensiveItem.cpp
@@ -1,11 +1,5 @@
 #include "DefensiveItem.h"
 #include "Utility.h"
-// #include "Dwarf.h"
-// #include "Paladin.h"
-// #include "DragonSlayer.h"
-// #include "Dragon.h"
-
-
 
 void DefensiveItem::use(Character* character)
 {
--- Paladin.cpp
+++ Paladin.cpp
@@ -1,7 +1,7 @@
 #include "Paladin.h"
 #include "Utility.h"
 
-Paladin::Paladin(std::string name_, int hp_, int armor_) : Character(hp_, armor_, 4), name(name_)
+Paladin::Paladin(std::string name_, int hp_, int armor_) : Character(hp_, armor_, 10), name(name_)
 {
     helpfulItems = makeHelpfulItems(rand() % 10);
     defensiveItems = makeDefensiveItems(rand() % 10);
